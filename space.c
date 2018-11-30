052b3faf7249999c082c62b371a5e512e71bbbe9
iahmedbacha <fi_ahmed_bacha@esi.dz>
2018-11-30 04:02:38 +0100

    feat: space functions added
    space functions added. Space important in the opening

--- space.c
+++ space.c
@@ -18,9 +18,39 @@
 #include "threats.h"
 
 double space_area (Pos* pos, Square* square, void* param) {
-    return 0;
+    if (square == NULL) {
+        return sum(pos, space_area, NULL);
+    }
+    double v = 0;
+    double r = rank(pos, square, NULL);
+    double f = file(pos, square, NULL);
+    if ((r >= 2 && r <= 4 && f >= 3 && f <= 6) && (board(pos, square->x ,square->y) != 'P') && (board(pos, square->x - 1 ,square->y - 1) != 'p') && (board(pos, square->x + 1 ,square->y - 1) != 'p')) {
+        v++;
+        if (board(pos, square->x, square->y - 1) == 'P' || board(pos, square->x, square->y - 2) == 'P' || board(pos, square->x, square->y - 3) == 'P') {
+            v++;
+        }
+    }
+    return v;
 }
 
 double space (Pos* pos, Square* square) {
-    return 0;
+    if (non_pawn_material(pos, NULL, NULL) + non_pawn_material(colorflip(pos), NULL, NULL) < 12222) {
+        return 0;
+    }
+    double weight = 0;
+    for (int x = 0; x < 8; x++) {
+        int open = TRUE;
+        for (int y = 0; y < 8; y++) {
+            if (indexOf("PNBRQK", 6, board(pos, x, y)) >= 0) {
+                weight++;
+            }
+            if (toupper(board(pos, x, y)) == 'P' ) {
+                open = FALSE;
+            }
+        }
+        if (open) {
+            weight -= 2;
+        }
+    }
+    return space_area(pos, NULL, NULL) * weight * weight / 16;
 }
